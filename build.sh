fb9bf622ee1afccdd741676ac2256158d1fba75e
Sayan Chakraborty <140915909+ugs196@users.noreply.github.com>
2025-06-30 14:39:37 +0530

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Step 1: Install normal packages
+pip install -r requirements.txt
+
+# Step 2: Install pyhoroscope from GitHub
+pip install git+https://github.com/tapaswenipathak/pyhoroscope.git
